e989d1341d3b272c3c4df532b4ce94fce197bf83
Nguyentienhao <haokn97@gmail.com>
2018-09-15 23:35:43 +0700

    complete agorithms install binary search tree

--- APIJava/Search/BinarySearch/BST.java
+++ APIJava/Search/BinarySearch/BST.java
@@ -1,5 +1,7 @@
+import java.security.Key;
 import java.util.NoSuchElementException;
-import java.util.Queue;
+
+import com.sun.corba.se.impl.orbutil.graph.Node;
 
 public class BST<Key extends Comparable<Key>, Value > {
     public Node root;
@@ -283,16 +285,267 @@ public class BST<Key extends Comparable<Key>, Value > {
             return x.key;
     }
 
+     /**
+     * Returns the smallest key in the symbol table greater than or equal to {@code key}.
+     *
+     * @param  key the key
+     * @return the smallest key in the symbol table greater than or equal to {@code key}
+     * @throws NoSuchElementException if there is no such key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+
+    public Key ceilling(Key key) {
+        if (key == null)
+            throw new IllegalArgumentException("argument to celling() is null");
+        if (isEmpty())
+            throw new NoSuchElementException("call celling() with empty symbol table");
+        Node x = ceilling(root, key);
+        if (x == null)
+            return null;
+        else 
+            return x.key;
+    } 
+
+    private Node ceilling(Node x, Key key) {
+        if (x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0)
+            return x;
+        if (cmp < 0) {
+            Node t = celling(x.left, key);
+            if (t != null)
+                return t;
+            else
+                return x;
+        }
+        return ceilling(x.right, key);
+        
+    }
+
+     /**
+     * Return the key in the symbol table whose rank is {@code k}.
+     * This is the (k+1)st smallest key in the symbol table.
+     *
+     * @param  k the order statistic
+     * @return the key in the symbol table of rank {@code k}
+     * @throws IllegalArgumentException unless {@code k} is between 0 and
+     *        <em>n</em>–1
+     */
+
+    public Key select(int k) {
+        if (k < 0 || k >= size) {
+            throw new IllegalArgumentException("argument to select() is invalid " + k);
+        }
+        Node x = select(root, k);
+        return x.key;
+    }
+
+    // Return key of rank k.
+    private Node select(Node x, int k) {
+        if (x == null) return null;
+        int t = size(x.left);
+        if (t > k)
+            return select(x.left, k);
+        else if (t < k)
+            return select(x.right, k-t-1);
+        else 
+            return x;
+    }
+
+     /**
+     * Return the number of keys in the symbol table strictly less than {@code key}.
+     *
+     * @param  key the key
+     * @return the number of keys in the symbol table strictly less than {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+
+     public int rank(Key key) {
+        if (key == null)
+           throw new IllegalArgumentException("argument to rank() is null");
+        return rank(key, root); 
+     }
+
+     // Number of keys in subtree less than key
+     private int rank(Key key, Node x) {
+        if (x == null)
+            return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)
+            return rank(key, x.left);
+        else if (cmp > 0)
+            return 1 + size(x.left) + rank(key, x.right);
+        else 
+            return size(x.left); 
+     }
+
+     /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in the symbol table
+     */
+
+    public Iterable<Key> keys() {
+        if (isEmpty())
+           return new Queue<Key>();
+        return keys(min(), max());        
+    }
+
+    /**
+     * Returns all keys in the symbol table in the given range,
+     * as an {@code Iterable}.
+     *
+     * @param  lo minimum endpoint
+     * @param  hi maximum endpoint
+     * @return all keys in the symbol table between {@code lo} 
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        if (lo == null)
+            throw new IllegalArgumentException("first argument to keys() is null");
+        if (hi == null)
+            throw new IllegalArgumentException("second argument to keys() is null");
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue;
+    } 
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null)
+            return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if ((cmplo <= 0) && (cmphi >= 0))
+            queue.enqueue(x.key);
+        if (cmphi > 0)
+            keys(x.right, queue, lo, hi);
+    }
+
+    /**
+     * Returns the number of keys in the symbol table in the given range.
+     *
+     * @param  lo minimum endpoint
+     * @param  hi maximum endpoint
+     * @return the number of keys in the symbol table between {@code lo} 
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+    public int size(Key lo, Key hi) {
+        if (lo == null)
+            throw new IllegalArgumentException("first argument to size() is null");
+        if (hi == null)
+            throw new IllegalArgumentException("second argument to size() is null");
+        if (lo.compareTo(hi) > 0)
+            return 0;
+        if (contains(hi))
+            return rank(hi) - rank(lo) + 1;
+        else 
+            return rank(hi) - rank(lo);            
+    }
+
+    /**
+     * Returns the height of the BST (for debugging).
+     *
+     * @return the height of the BST (a 1-node tree has height 0)
+     */
+
+    public int height() {
+        return height(root);
+    } 
+
+    private int height(Node x) {
+        if (x == null)
+            return -1;
+        return 1 + Math.max(height(x.left), height(x.right));
+    }
+
     /**
      * Returns the keys in the BST in level order (for debugging).
      *
      * @return the keys in the BST in level order traversal
      */
-    // public Iterable<Key> levelOrder() {
-    //     Queue<Key> keys = new Queue<Key>();
-    //     Queue<Node> queue = new Queue<Node>();
-    //     queue.enqueue();
 
-    //     return keys;
-    // }
+    public Iterable<Key> levelOrder() {
+        Queue<Key> keys = new Queue<Key>();
+        Queue<Node> queue = new Queue<Node>();
+        queue.enqueue(root);
+        while (queue.isEmpty()) {
+            Node x = queue.dequeue();
+            if (x == null)
+                continue;
+            keys.enqueue(x.key);
+            queue.enqueue(x.left);
+            queue.enqueue(x.right);            
+        }
+        return keys;
+    } 
+
+    /*************************************************************************
+    *  Check integrity of BST data structure.
+    ***************************************************************************/
+    private boolean check() {
+        if (!isBST())
+    }
+
+    /**
+     * does this binary tree satisfy symmetric order?
+     * Note: this test also ensures that data structure is a binary tree since order is strict
+     */
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    /**
+     * + is the tree rooted at x a BST with all keys strictly between min and max
+     * + (if min or max is null, treat as empty constraint)
+     * + Credit: Bob Dondero's elegant solution
+     */
+
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null)
+            return true;
+        if (min != null && x.key.compareTo(min) <= 0) 
+            return false;
+        if (max != null && x.key.compareTo(max) >= 0)
+            return false;
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    } 
+
+    /**
+     * are the size fields correct?
+     */
+    private boolean isSizeConsistent() {
+        return isSizeConsistent(root);
+    }
+    private boolean isSizeConsistent(Node x) {
+        if (x == null)
+            return true;
+        if (x.size != size(x.left) + size(x.right) + 1)
+            return false;
+        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+    }
+
+    // check that ranks are consistent
+    private boolean isRankConsistent() {
+        for (int i = 0; i < size(); i++)
+            if (i != rank(select(i))) return false;
+        for (Key key : keys())
+            if (key.compareTo(select(rank(key))) != 0) return false;
+        return true;
+    }
+
+    /**
+     * Returns the keys in the BST in level order (for debugging).
+     *
+     * @return the keys in the BST in level order traversal
+     */
 }
